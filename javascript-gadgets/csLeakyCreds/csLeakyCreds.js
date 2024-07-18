163e70281b4cd886227191aa80afc1ce35f5ad38
Colin Rubbert <colinrubbert@users.noreply.github.com>
2024-07-18 17:00:39 +0000

    Work on csleaky creds

--- javascript-gadgets/csLeakyCreds/csLeakyCreds.js
+++ javascript-gadgets/csLeakyCreds/csLeakyCreds.js
@@ -3,3 +3,19 @@
 // Chromium DevTools regex for leaked credentials
 
 window.__csLeakyCreds = "(access_key|access_token|admin_pass|admin_user|AKIA|algolia_admin_key|algolia_api_key|alias_pass|alicloud_access_key|amazon_secret_access_key|amazonaws|ansible_vault_password|aos_key|api_key|api_key_secret|api_key_sid|api_secret|api-key|api.googlemaps AIza|AIza|apidocs|apikey|apiSecret|app_debug|app_id|appkey|app_key|app_log_level|app_secret|appkey|appkeysecret|application_key|appsecret|appspot|auth_token|authtoken|authorizationToken|authorization|authentication|authsecret|aws_access|aws_access_key_id|aws_bucket|aws_key|aws_secret|aws_secret_key|aws_token|AWSSecretKey|b2_app_key|bashrc password|bearer|bintray_apikey|bintray_gpg_password|bintray_key|bintraykey|bluemix_api_key|bluemix_pass|browserstack_access_key|bucket_password|bucketeer_aws_access_key_id|bucketeer_aws_secret_access_key|built_branch_deploy_key|bx_password|cache_driver|cache_s3_secret_key|cattle_access_key|cattle_secret_key|certificate_password|ci_deploy_password|client_secret|client_zpk_secret_key|clojars_password|cloud_api_key|cloud_watch_aws_access_key|cloudant_password|cloudflare_api_key|cloudflare_auth_key|cloudinary_api_secret|cloudinary_name|codecov_token|\.config|config\.|conn.login|connectionstring|consumer_key|consumer_secret|credentials|cypress_record_key|database_password|database_schema_test|datadog_api_key|datadog_app_key|datadog_token|datadog_rum_token|db_password|db_server|db_username|dbpasswd|dbpassword|dbuser|deploy_password|digitalocean_ssh_key_body|digitalocean_ssh_key_ids|docker_hub_password|docker_key|docker_pass|docker_passwd|docker_password|dockerhub_password|dockerhubpassword|dot-files|dotfiles|droplet_travis_password|dynamoaccesskeyid|dynamosecretaccesskey|elastica_host|elastica_port|elasticsearch_password|encryption_key|encryption_password|env.heroku_api_key|env.sonatype_password|eureka.awssecretkey|firebase|gh_token|github_key|github_token|github_key|gitlab|isAdmin|master_key|slack_api|slack_token|swagger|private_key|xoxb|xoxp|config|admin)";
+
+window.__csLeakyCreds_all = ""
+
+window.__csLeakyCreds_app_env_config = ""
+
+window.__csLeakyCreds_auth = ""
+
+window.__csLeakyCreds_cloud_api = ""
+
+window.__csLeakyCreds_database_storage = ""
+
+window.__csLeakyCreds_encryption_security = ""
+
+window.__csLeakyCreds_misc = ""
+
+
