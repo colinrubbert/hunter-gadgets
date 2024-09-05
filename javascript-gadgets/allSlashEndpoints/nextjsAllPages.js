9c14288e6ba778f28636bd05cdbda631089a778b
Colin Rubbert <colinrubbert@users.noreply.github.com>
2024-09-05 15:16:41 -0700

    Create nextjsAllPages.js

---
+++ javascript-gadgets/allSlashEndpoints/nextjsAllPages.js
@@ -0,0 +1,8 @@
+// Create a list of all Next.JS '/' endpoints from your current DOM and from all external script sources embedded on the page
+// Credit: @renniepak - https://x.com/i/bookmarks?post_id=1830963129897124294
+
+function getAllNextJsPages {
+  let pages = __BUILD_MANIFEST.sortedPages.join('\n');
+
+  console.log(pages);
+}
