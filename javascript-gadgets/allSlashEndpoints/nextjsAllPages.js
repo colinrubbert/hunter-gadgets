40acdca8df46b9c5d678de5c2c31fc76b9a5a26c
Colin Rubbert <colinrubbert@users.noreply.github.com>
2024-09-13 14:08:14 -0700

    Update nextjsAllPages.js
    Fix typo

--- javascript-gadgets/allSlashEndpoints/nextjsAllPages.js
+++ javascript-gadgets/allSlashEndpoints/nextjsAllPages.js
@@ -1,8 +1,7 @@
 // Create a list of all Next.JS '/' endpoints from your current DOM and from all external script sources embedded on the page
 // Credit: @renniepak - https://x.com/i/bookmarks?post_id=1830963129897124294
 
-function getAllNextJsPages {
-  let pages = __BUILD_MANIFEST.sortedPages.join('\n');
-
+function getAllNextJsPages() {
+  let pages = __BUILD_MANIFEST__.sortedPages.join('\n');
   console.log(pages);
 }
